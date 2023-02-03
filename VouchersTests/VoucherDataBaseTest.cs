96cc1544d57180d71f55a517ca37a80a3d4cdb99
mo <mo@DESKTOP-B6TCKIB>
2023-02-03 08:04:54 +0330

    test save voucher and ef core by sqllite

--- VouchersTests/VoucherDataBaseTest.cs
+++ VouchersTests/VoucherDataBaseTest.cs
@@ -2,6 +2,7 @@
 using AccountingVoucher.Domain.Entities;
 using AccountingVoucher.Infrastructure;
 using AccountingVoucher.Infrastructure.Repositories;
+using AutoFixture;
 using FrameWork.Infrastruture.Persistance;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
@@ -17,11 +18,14 @@ namespace VouchersTests
 	public class VoucherDataBaseTest
 	{
 
-
+		Mock<IVoucherRepository>? _voucherRepositoryMock;
+		Mock<IRepositoryBase<Voucher>>? _repositryBase;
 		public  DbContextOptions<VoucherContext> Seup()
 		{
 			var connectionStringBuilder = new SqliteConnectionStringBuilder() { DataSource = ":memory" };
 			var connection = new SqliteConnection(connectionStringBuilder.ToString());
+			_voucherRepositoryMock = new Mock<IVoucherRepository>();
+			_repositryBase = new Mock<IRepositoryBase<Voucher>>();
 			return new DbContextOptionsBuilder<VoucherContext>().UseSqlite().Options; ;
 		}
 		[Fact]
@@ -34,13 +38,51 @@ namespace VouchersTests
 				context.Database.OpenConnection();
 				context.Database.EnsureCreated();
 		
-			var repositryBase = new Mock<IRepositoryBase<Voucher>>();
-			var	_voucherRepositoryMock = new Mock<IVoucherRepository>();
-			var voucherrepository = new VoucherRepository(repositryBase.Object, context);
+			 _repositryBase = new Mock<IRepositoryBase<Voucher>>();
+
+			var voucherrepository = new VoucherRepository(_repositryBase.Object, context);
 
 		    await    Assert.ThrowsAsync<ArgumentException>(() => voucherrepository.GetVoucherByVoucherNumber(0));
 
 			}
+		}
+
+		[Fact]
+		public void DescriptionShouldBe_Saved_AsExpected()
+		{
+			var option = Seup();
+
+			using (var context = new VoucherContext(option))
+			{
+
+				context.Database.OpenConnection();
+				context.Database.EnsureCreated();
+				var fixture = new Fixture();
+				var sut = fixture.Build<Voucher>()
+					.FromFactory<int>((x) => new Voucher(GetVoucherSnapShotList(), "decription"))
+					.Create();
+
+				context.Voucher.Add(sut);
+				context.SaveChanges();
+				_repositryBase = new Mock<IRepositoryBase<Voucher>>();
+				var voucherrepository = new VoucherRepository(_repositryBase.Object, context);
+				var voucher = voucherrepository.GetVoucherByVoucherNumber(sut.VoucherNumber);
+
+				Assert.Equal("decription", voucher.Result.Description.Value);
+
+
+			}
+		}
+
+		public List<VoucherSnapShot> GetVoucherSnapShotList()
+		{
+			var fixture = new Fixture();
+			var lst = new List<VoucherSnapShot>();
+			var fixtureSnapshotSUT = fixture.Build<VoucherSnapShot>().With(t => t.Price, 2322).With(t => t.VoucherItemType, 1).Create();
+			var fixtureSnapshotSUTTwo = fixture.Build<VoucherSnapShot>().With(t => t.Price, 43434).With(t => t.VoucherItemType, 3).Create();
+			lst.Add(fixtureSnapshotSUT);
+			lst.Add(fixtureSnapshotSUTTwo);
+			return lst;
 		}
 	}
 }
